930f59ffc5b017cee89094914b956ea47894060b
orenono <orenlahav@yahoo.com>
2015-05-11 22:17:13 -0400

    it kinda works

--- NetworkedObjects/src/wsMessages/ClientConnectedMessage.java
+++ NetworkedObjects/src/wsMessages/ClientConnectedMessage.java
@@ -10,19 +10,3 @@ public class ClientConnectedMessage extends Message {
 	public int getClientsCount() { return this.clientsCount; }
 }
 
-/**
- * add shipMove message
- * add shipMoveMessage Encoder
- * 
- * add implemantaion to message decoder (if else)
- * 
- * in server endpoint and client endpoint - add encoder in the annotaions.
- * 
- * somehow gamePanel need to know that ship has moved and send a message back to server that the ship moved.
- * 
- * client endpoint need to listen to the message and notify gamepanel which in turn should paint the movement of the ship
- * 
- * 
- * 
- * 
- */
